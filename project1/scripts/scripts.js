143d872af3393c3d55fa758b1f15b74016c73d45
Ireneusz Luczak <ericluczak@Ireneuszs-MacBook-Pro.local>
2015-07-16 16:00:21 -0500

    commitgit add .git add .

--- project1/scripts/scripts.js
+++ project1/scripts/scripts.js
@@ -1,3 +1,11 @@
+//https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key=571CCF8D3B029DEBB4EF89B95B08D6DF&account_id=35634853&matches_requested=1
+//thats the link I would use to retrieve my last game of Dota 2 played.
+// need to allow access through dota 2 client as well.
+
+
+
+
+
 $(document).ready(function() {
 
   console.log("All resources are loaded");
@@ -59,3 +67,38 @@ app.sendEmail = function (emailData) {
       }
     }
   };
+
+
+// This is the steam call for 1 game. I need to allow access from my dota 2 client at home.
+
+
+// var steamAjaxCall = function () {
+// var url = 'https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key=571CCF8D3B029DEBB4EF89B95B08D6DF&account_id=35634853&matches_requested=1';
+//
+// $.getJSON("http://query.yahooapis.com/v1/public/yql",
+// {
+// q: "select * from json where url=\"" + url + "\"",
+// format: "json"
+// },
+// function(data){
+//    console.log(data.query.results.result);
+// })
+// }
+// steamAjaxCall ();
+
+
+
+
+// // parser for steam time.
+// function timeConverter(UNIX_timestamp){
+//   var a = new Date(UNIX_timestamp * 1000);
+//   var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+//   var year = a.getFullYear();
+//   var month = months[a.getMonth()];
+//   var date = a.getDate();
+//   var hour = a.getHours();
+//   var min = a.getMinutes();
+//   var sec = a.getSeconds();
+//   var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+//   return time;
+// }
